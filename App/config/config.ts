39643adfa56407ef630bdd1835087436200acd19
KostiaSA <sonyk795>
2017-05-31 15:54:05 +0300

    --

--- App/config/config.ts
+++ App/config/config.ts
@@ -4,14 +4,14 @@ export interface IConfig {
     apkUrl: string;
 }
 
-let developDir: IConfig = {
-    appName: "Buhta Player 1.0",
-//    apiUrl: "http://5.19.239.191:3000/api",
-//    apkUrl: "http://5.19.239.191:3000/downloads/r.apk",
-    apiUrl: "http://192.168.0.14:3001/api",
-    apkUrl: "http://192.168.0.14:3001/downloads/BuhtaPlayer.apk",
-}
-
+// let developDir: IConfig = {
+//     appName: "Buhta Player 1.0",
+// //    apiUrl: "http://5.19.239.191:3000/api",
+// //    apkUrl: "http://5.19.239.191:3000/downloads/r.apk",
+//     apiUrl: "http://192.168.0.14:3001/api",
+//     apkUrl: "http://192.168.0.14:3001/downloads/BuhtaPlayer.apk",
+// }
+//
 let cloudDir: IConfig = {
     appName: "Buhta Player 1.0",
     apiUrl: "http://player.buhta.ru/api",
@@ -19,4 +19,4 @@ let cloudDir: IConfig = {
 }
 
 
-export let config: IConfig = developDir;
+export let config: IConfig = cloudDir;
